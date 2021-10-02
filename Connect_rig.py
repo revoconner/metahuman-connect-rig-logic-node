af5de40e2130b4bdd6aa8dd5ad5d25082357e43e
Rév O'Conner <88772846+revoconner@users.noreply.github.com>
2021-10-02 17:25:44 +0530

    Update Connect_rig.py

--- Connect_rig.py
+++ Connect_rig.py
@@ -1,9 +1,12 @@
 from maya import cmds
 import pymel.core as pm
 
+
 cmds.createEmbeddedNodeRL4(
-n="rigLogicNode", 
-dfp="C:\Users\Rev Oconner\Desktop\Zoe01\Maya\Journalist\scripts\Kristofer_rl.dna",
+n="rigLogicNode", #name your rig logic node filename here, the node will have the same name
+
+dfp_path="C:\Maya\Journalist\scripts\Kristofer_rl.dna", #enter the path to the file here, no need to change backslash in windows. 
+dfp=dfp_path.replace("\\", "/")
 jn="DHIhead:<objName>.<attrName>", 
 amn="FRM_WMmultipliers.<objName>_<attrName>", 
 bsn="<objName>_blendShapes.<attrName>", 
